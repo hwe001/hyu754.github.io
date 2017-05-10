f3b34301bb9e0666c75ed8654bc039545d2cf8d0
hyu754 <hyu754@aucklanduni.ac.nz>
2017-05-10 18:01:32 +1200

    initial commit

---
+++ js/verticle_menu.js
@@ -0,0 +1,9 @@
+  //When the open button is pressed
+function openNav(){
+    document.getElementById("mySidenav").style.width = "290px";
+}
+
+//Closing the side navigation
+function closeNav(){
+    document.getElementById("mySidenav").style.width="0px";
+}
