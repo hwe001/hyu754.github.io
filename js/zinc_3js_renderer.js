4d3a3996684b812e136201a083d4eb90f026afdd
hyu754 <hyu754@aucklanduni.ac.nz>
2017-05-16 17:51:23 +1200

    back dominant camera working

--- js/zinc_3js_renderer.js
+++ js/zinc_3js_renderer.js
@@ -1203,8 +1203,9 @@ Zinc.Scene = function ( containerIn, rendererIn) {
             if(backcameraselected==false){
                 
                 if  (document.getElementById("videoSource").length>1){
-                  var theSelect = document.getElementById('videoSource');
-                var lastValue = theSelect.options[theSelect.options.length - 1].value;
+                alert(document.querySelector('select#videoSource')[1].value);
+                    
+                   document.querySelector('select#videoSource').value =document.querySelector('select#videoSource')[1].value;
                     backcameraselected =true;
                     start();
                 }
