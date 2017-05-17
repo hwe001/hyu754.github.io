74cdbad3e7babda1ed87cc20d80997c6e447a115
hyu754 <hyu754@aucklanduni.ac.nz>
2017-05-17 15:51:54 +1200

    ?bugs

--- js/create_video.js
+++ js/create_video.js
@@ -166,6 +166,10 @@ var checkOrientation = function(){
             var dummyGlobalHeight = global_height;
             global_height = global_width;
             global_width = dummyGlobalHeight;
+            
+            if((previousOrientation==90)||(previousOrientation==-90)){
+                document.getElementById('plane-slider').value = 300;
+            }
         }
     }
 }
